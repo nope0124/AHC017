5d011d76fcb471caf77447e7040b9e0a72f074c1
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-01-29 15:04:30 +0900

    random_sampleの実装

--- main.cpp
+++ main.cpp
@@ -3,25 +3,22 @@
 using namespace std;
 typedef long long int ll;
 typedef long double ld;
-typedef pair<int, int> pint;
-
-
+typedef pair<ll, ll> pll;
 
 typedef enum
 {
-    Unconstructed,
-    Occupied,
-    Constructed,
+    Unconstructed, // 未工事状態
+    Occupied,      // 現在道路として使われている
+    Constructed,   // 工事完了
 } Road_Status;
 
-short N, M, D, K;
-
+ll N, M, D, K;
 
 struct Edge {
-    short edge_id;
-    short from;
-    short to;
-    int cost;
+    ll edge_id;
+    ll from;
+    ll to;
+    ll cost;
     Road_Status road_status = Unconstructed;
     bool operator==(const Edge& other) { return edge_id == other.edge_id; }
     bool operator!=(const Edge& other) { return edge_id != other.edge_id; }
@@ -56,39 +53,39 @@ struct Edge {
 struct Dijkstra {
     private:
         struct Dijkstra_Edge {
-            short to;
-            int cost;
-            Dijkstra_Edge(short _to, int _cost) {
+            ll to;
+            ll cost;
+            Dijkstra_Edge(ll _to, ll _cost) {
                 to = _to;
                 cost = _cost;
             }
         };
         vector<Dijkstra_Edge> graph[1010];
-        vector<int> dist;
+        vector<ll> dist;
 
     public:
         
         Dijkstra(vector<Edge> edge_list) {
-            for(short i = 0; i < edge_list.size(); i++) {
+            for(ll i = 0; i < edge_list.size(); i++) {
                 graph[edge_list[i].from].push_back(Dijkstra_Edge(edge_list[i].to, edge_list[i].cost));
                 graph[edge_list[i].to].push_back(Dijkstra_Edge(edge_list[i].from, edge_list[i].cost));
             }
-            for(short i = 0; i < N; i++) dist.push_back(1000000000);
+            for(ll i = 0; i < N; i++) dist.push_back(1000000000);
         }
         
-        vector<int> get_dist(int s) {
-            priority_queue<pint, vector<pint>, greater<pint> > que;
-            for(short i = 0; i < N; i++) dist[i] = 1000000000;
+        vector<ll> get_dist(ll s) {
+            priority_queue<pll, vector<pll>, greater<pll> > que;
+            for(ll i = 0; i < N; i++) dist[i] = 1000000000;
             dist[s] = 0;
-            que.push(pint(0, s));
+            que.push(pll(0, s));
             while(!que.empty()){
-                pint p = que.top(); que.pop();
-                int v = p.second;
+                pll p = que.top(); que.pop();
+                ll v = p.second;
                 if(dist[v] < p.first) continue;
                 for(Dijkstra_Edge e: graph[v]){
                     if(dist[e.to] > dist[v] + e.cost){
                         dist[e.to] = dist[v] + e.cost;
-                        que.push(pint(dist[e.to], e.to));
+                        que.push(pll(dist[e.to], e.to));
                     }
                 }
             }
@@ -99,22 +96,22 @@ struct Dijkstra {
 
 struct UnionFind {
     private:
-        vector<short> parent;
+        vector<ll> parent;
     
     public:
-        UnionFind(short n) : parent(n, -1) { }
-        void init(short n) { parent.assign(n, -1); }
+        UnionFind(ll n) : parent(n, -1) { }
+        void init(ll n) { parent.assign(n, -1); }
     
-        int root(short x) {
+        ll root(ll x) {
             if(parent[x] < 0) return x;
             else return parent[x] = root(parent[x]);
         }
     
-        bool issame(short x, short y) {
+        bool issame(ll x, ll y) {
             return root(x) == root(y);
         }
     
-        void merge(short x, short y) { //親、子
+        void merge(ll x, ll y) { //親、子
             x = root(x);
             y = root(y);
             if(x == y) return;
@@ -123,59 +120,71 @@ struct UnionFind {
             parent[y] = x; // 大きい木の根に小さい木をつける, yの親はx
         }
         
-        short size(short x) {
+        ll size(ll x) {
             return -parent[root(x)];
         }
 
 };
 
 
+template <typename T> vector<T> random_sample(vector<T> population, int k) {
+    if(population.size() < k) {
+        cerr << "Error: population.size() < k" << endl;
+        return population;
+    }
+    int num = population.size() - k;
+    for(int i = 0; i < num; i++) {
+        int idx = rand()%population.size();
+        population.erase(population.begin() + idx);
+    }
+    return population;
+}
 
-vector<vector<int> > compute_dist_matrix(vector<Edge> edge_list, vector<short> ans, short day) {
+
+
+vector<vector<ll> > compute_dist_matrix(vector<Edge> edge_list, vector<ll> ans, ll day) {
     vector<Edge> new_edge_list;
-    for(short i = 0; i < M; i++) {
+    for(ll i = 0; i < M; i++) {
         if(ans[edge_list[i].edge_id] == day) continue;
         new_edge_list.push_back(edge_list[i]);
     }
     Dijkstra djk = Dijkstra(new_edge_list);
-    vector<vector<int> > dist;
-    for(short s = 0; s < N; s++) {
+    vector<vector<ll> > dist;
+    for(ll s = 0; s < N; s++) {
         dist.push_back(djk.get_dist(s));
     }
     return dist;
 }
 
 
-
-
-ll compute_score(vector<short> ans, vector<Edge> edge_list) {
-    vector<short> cnt = vector<short>(D + 1, 0);
-    for(short i = 0; i < M; i++) {
+ll compute_score(vector<ll> ans, vector<Edge> edge_list) {
+    vector<ll> cnt = vector<ll>(D + 1, 0);
+    for(ll i = 0; i < M; i++) {
+        if(ans[i] == 0) return -1;
         cnt[ans[i]] += 1;
     }
-    for(short i = 1; i <= D; i++) {
+    for(ll i = 1; i <= D; i++) {
         if(cnt[i] > K) {
             return -1;
         }
     }
     ll num = 0;
-    vector<vector<int> > dist0 = compute_dist_matrix(edge_list, ans, 0);
+    ll penalty_count = 0;
+    vector<vector<ll> > dist0 = compute_dist_matrix(edge_list, ans, 0);
     vector<ll> fs;
-    for(short d = 1; d <= D; d++) {
-        vector<vector<int> > dist = compute_dist_matrix(edge_list, ans, d);
+    for(ll d = 1; d <= D; d++) {
+        vector<vector<ll> > dist = compute_dist_matrix(edge_list, ans, d);
         ll tmp = 0;
-        for(short i = 0; i < N; i++) {
-            for(short j = i + 1; j < N; j++) {
-                // if(i == 0) cerr << dist[i][j] << endl;
+        for(ll i = 0; i < N; i++) {
+            for(ll j = i + 1; j < N; j++) {
+                if(dist[i][j] == 1000000000) penalty_count++;
                 tmp += (dist[i][j] - dist0[i][j]);
             }
         }
         num += tmp;
-        
         fs.push_back(tmp / (N * (N - 1) / 2));
-        // cerr << fs.back() << endl;
     }
-    
+    if(penalty_count > 0) cerr << "Warning: penalty_count > 0" << endl;
     ll den = D * N * (N - 1) / 2;
     ld avg = (ld)num / den * 1000.0;
     return round(avg);
@@ -185,9 +194,9 @@ ll compute_score(vector<short> ans, vector<Edge> edge_list) {
 int main() {
     cin >> N >> M >> D >> K;
     vector<Edge> edge_list(M);
-    vector<short> unconstructed_edge_list(M);
-    vector<short> ans(M);
-    for(short i = 0; i < M; i++) {
+    vector<ll> unconstructed_edge_list(M);
+    vector<ll> ans(M);
+    for(ll i = 0; i < M; i++) {
         edge_list[i].edge_id = i;
         cin >> edge_list[i].from >> edge_list[i].to >> edge_list[i].cost;
         edge_list[i].from--;
@@ -195,11 +204,10 @@ int main() {
         unconstructed_edge_list[i] = edge_list[i].edge_id;
     }
     sort(edge_list.begin(), edge_list.end());
-    short unconstructed_number = M;
-    for(short d = 0; d < D; d++) {
+    for(ll d = 0; d < D; d++) {
         UnionFind uf = UnionFind(N);
         
-        for(short i = 0; i < M; i++) {
+        for(ll i = 0; i < M; i++) {
             if(edge_list[i].road_status == Constructed) {
                 uf.merge(edge_list[i].from, edge_list[i].to);
             }else {
@@ -207,7 +215,7 @@ int main() {
             }
         }
 
-        for(short i = 0; i < M; i++) {
+        for(ll i = 0; i < M; i++) {
             if(edge_list[i].road_status == Constructed) continue;
             if(uf.issame(edge_list[i].from, edge_list[i].to)) continue;
             uf.merge(edge_list[i].from, edge_list[i].to);
@@ -216,25 +224,24 @@ int main() {
         
 
         vector<Edge*> unconstructed_edge;
-        for(short i = 0; i < M; i++) {
+        for(ll i = 0; i < M; i++) {
             if(edge_list[i].road_status != Unconstructed) continue;
             unconstructed_edge.push_back(&edge_list[i]);
         }
-        short unconstructed_edge_number = unconstructed_edge.size();
-        for(short i = 0; i < min(unconstructed_edge_number, K); i++) {
-        // for(short i = 0; i < min((int)unconstructed_edge_number, (M + D - 1) / D); i++) {
-            short idx = rand()%unconstructed_edge.size();
-            unconstructed_edge[idx]->road_status = Constructed;
-            ans[unconstructed_edge[idx]->edge_id] = d + 1;
-            unconstructed_edge.erase(unconstructed_edge.begin() + idx);
+
+        unconstructed_edge = random_sample(unconstructed_edge, min((ll)unconstructed_edge.size(), K));
+
+        for(ll i = 0; i < unconstructed_edge.size(); i++) {
+            unconstructed_edge[i]->road_status = Constructed;
+            ans[unconstructed_edge[i]->edge_id] = d + 1;
         }
     }
     
-    for(short i = 0; i < M; i++) {
+    for(ll i = 0; i < M; i++) {
         cout << ans[i] << " \n"[i == M - 1];
     }
 
-    // ll score = compute_score(ans, edge_list);
-    // cerr << score << endl;
+    ll score = compute_score(ans, edge_list);
+    cerr << score << endl;
 
 }
